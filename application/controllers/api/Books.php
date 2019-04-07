014a4b29933ea51e5e9c0a4230d28be85bedf6c4
Andi WawanK <andifirwansyah250@gmail.com>
2019-04-07 19:17:36 +0700

    a simple restfull api

--- application/controllers/api/Books.php
+++ application/controllers/api/Books.php
@@ -16,7 +16,7 @@ class Books extends REST_Controller{
         
         // $id = $this->get('id');
 
-        if($id === null){
+        if($id === NULL){
             $books = $this->BooksModel->getBooks();
         }else{
             $books = $this->BooksModel->getBooks($id);
@@ -35,4 +35,68 @@ class Books extends REST_Controller{
         }
 
     }
+    function index_post(){
+        $data = [
+            'Judul' => $this->post('judul'),
+            'Penerbit' => $this->post('penerbit'),
+            'Kategori' => $this->post('kategori'),
+            'Harga' => $this->post('harga')
+        ];
+        
+        if($this->BooksModel->addBooks($data) > 0){
+            $this->response([
+                'status' => true,
+                'message' => 'New Books has been created!'
+            ], REST_Controller::HTTP_CREATED);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Failed to created new Books!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+
+    }
+    function index_put($id){
+        $data = [
+            'Judul' => $this->put('judul'),
+            'Penerbit' => $this->put('penerbit'),
+            'Kategori' => $this->put('kategori'),
+            'Harga' => $this->put('harga')
+        ];
+
+        if($this->BooksModel->updateBooks($data, $id) > 0){
+            $this->response([
+                'status' => true,
+                'message' => 'Books has been updated!'
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                'status' => false,
+                'message' => 'Failed to update Books!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    function index_delete($id = NULL){
+        if($id === NULL){
+            $this->response([
+                'status' => false,
+                'message' => 'Provide an ID!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }else{
+            if($this->BooksModel->deleteBooks($id) > 0){
+                $this->response([
+                    'status' => true,
+                    'id' => $id,
+                    'message' => 'Books was deleted!'
+                ], REST_Controller::HTTP_OK);
+            }else{
+                $this->response([
+                    'status' => false,
+                    'message' => 'ID Not Found!'
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+        }
+    }
+    
 }
--- application/models/BooksModel.php
+++ application/models/BooksModel.php
@@ -8,4 +8,17 @@ class BooksModel extends CI_Model{
             return $this->db->get_where('books',['ID_books' => $id])->result_array();
         }
     }
+    function addBooks($data){
+        $this->db->insert('books', $data);
+        return $this->db->affected_rows();
+    }
+    function updateBooks($data, $id){
+        // $this->db->where('ID_books', $id);
+        $this->db->update('books', $data, ['ID_books' => $id]);
+        return $this->db->affected_rows();
+    }
+    function deleteBooks($id){
+        $this->db->delete('books',['ID_books' => $id]);
+        return $this->db->affected_rows();
+    }
 }
